cfe980d8eb13b2b16612ebe6ded8bc4b5cb81e94
Enoch Boateng <boaten01@gmail.com>
2016-03-04 08:07:38 -0500

    Added missing Files (atlas, html, settings)

--- html/src/net/asg/games/dante/client/HtmlLauncher.java
+++ html/src/net/asg/games/dante/client/HtmlLauncher.java
@@ -3,17 +3,19 @@ package net.asg.games.dante.client;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
-import net.asg.games.dante.DantesEscape;
+
+import net.asg.games.dante.Constants;
+import net.asg.games.dante.DantesEscapeGame;
 
 public class HtmlLauncher extends GwtApplication {
 
         @Override
         public GwtApplicationConfiguration getConfig () {
-                return new GwtApplicationConfiguration(480, 320);
+                return new GwtApplicationConfiguration(Constants.GAME_WIDTH, Constants.GAME_HEIGHT);
         }
 
         @Override
         public ApplicationListener getApplicationListener () {
-                return new DantesEscape();
+                return new DantesEscapeGame();
         }
 }
--- ios/src/net/asg/games/dante/IOSLauncher.java
+++ ios/src/net/asg/games/dante/IOSLauncher.java
@@ -1,17 +1,16 @@
 package net.asg.games.dante;
 
-import org.robovm.apple.foundation.NSAutoreleasePool;
-import org.robovm.apple.uikit.UIApplication;
-
 import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
 import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
-import net.asg.games.dante.DantesEscape;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
 
 public class IOSLauncher extends IOSApplication.Delegate {
     @Override
     protected IOSApplication createApplication() {
         IOSApplicationConfiguration config = new IOSApplicationConfiguration();
-        return new IOSApplication(new DantesEscape(), config);
+        return new IOSApplication(new DantesEscapeGame(), config);
     }
 
     public static void main(String[] argv) {
