9f448ad6c90c09235d4e8219de3ac3a0b0aabe99
Enoch Boateng <boaten01@gmail.com>
2017-04-06 08:12:16 -0400

    New changelist

--- android/src/net/asg/games/dante/android/ActionResolverAndroid.java
+++ android/src/net/asg/games/dante/android/ActionResolverAndroid.java
@@ -6,8 +6,11 @@ import com.badlogic.gdx.Gdx;
 
 import net.asg.games.dante.ActionResolver;
 import net.asg.games.dante.DantesEscapeGame;
+import net.asg.games.dante.HighScoreManager;
 import net.asg.games.dante.screens.AbstractScreen;
 import net.asg.games.dante.screens.SettingsScreen;
+
+import java.util.List;
 import java.util.logging.Handler;
 import java.util.logging.LogRecord;
 
@@ -29,8 +32,33 @@ public class ActionResolverAndroid implements ActionResolver {
      * Implement interface back button
      */
     public void backButton(DantesEscapeGame game) {
-        System.out.println("Hello World from the Back button");
+        System.out.println("Hello GameWorld from the Back button");
         Gdx.app.debug("MyBackButton", "Hello world from the Back Button");
         game.gotoMainMenuScreen();
+    }
+
+    @Override
+    public void showAds(boolean show) {
+
+    }
+
+    @Override
+    public void showOrLoadInterstital() {
+
+    }
+
+    @Override
+    public void getHighScoreName() {
+
+    }
+
+    @Override
+    public void getDebugSetting() {
+
+    }
+
+    @Override
+    public void showScores(List<HighScoreManager.HighScore> highScores) {
+
     }
 }
